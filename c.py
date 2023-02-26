28284da2195bca8b4eb8c5925dd7572f955b9b6b
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-02-26 15:22:43 +0700

    Update c.py

--- c.py
+++ c.py
@@ -1,10 +1,10 @@
-import os
+
 from pyrogram import Client
 from pyromod import listen
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+API_ID = 19685518
+API_HASH = "33bf1d586e5fdfd9e66aaa52a576935a"
+BOT_TOKEN = "5568805542:AAGyhPEY1LLtMalwDU6HNXDC-DihRILi2VM"
 
 bot = Client(":memory:",
              api_id=API_ID,
